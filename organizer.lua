654e96598f3e04c59dacc4da77396a978021baa6
shastaxc <shastaxc@gmail.com>
2021-06-29 20:11:52 -0400

    Increment version

--- organizer.lua
+++ organizer.lua
@@ -38,7 +38,7 @@ slips = require 'slips'
 
 _addon.name = 'Organizer'
 _addon.author = 'Byrth, maintainer: Rooks, reworked by: Shasta'
-_addon.version = 2.20210628
+_addon.version = 2.20210629
 _addon.commands = {'organizer','org'}
 
 _static = {
