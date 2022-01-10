785bcaef744a04f9c6f0b4fbde6f4eaaa908db9e
shastaxc <shastaxc@gmail.com>
2022-01-09 20:39:30 -0500

    Remove debugging import

--- organizer.lua
+++ organizer.lua
@@ -35,7 +35,6 @@ require 'lists'
 require 'functions'
 config = require 'config'
 slips = require 'slips'
-inspect = require 'inspect'
 
 _addon.name = 'Organizer'
 _addon.author = 'Byrth, maintainer: Rooks, reworked by: Shasta'
